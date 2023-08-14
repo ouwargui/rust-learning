1e29dba05e00e1aceda1bcf6fd5ddf1a2d9e55c7
ouwargui <guikemon2@gmail.com>
2023-08-14 17:51:47 -0300

    feat: change colors for the buttons based on counter value

--- src/app.rs
+++ src/app.rs
@@ -17,12 +17,22 @@ struct GreetArgs<'a> {
 pub fn App(cx: Scope) -> impl IntoView {
     let (count, set_count) = create_signal(cx, 0);
 
-    let increment = move |_: MouseEvent| set_count.update(|v| *v += 1);
+    let handleBlueClass = move || count.get() % 2 == 1;
+    let handleRedClass = move || count.get() % 2 == 0;
 
     view! { cx,
-        <main class="bg-red-200 w-full min-h-screen flex flex-col items-center justify-center" >
-            <span>Counter = {count}</span>
-            <button on:click=increment class="border border-zinc-700 p-2 rounded bg-blue-300">Increment</button>
+        <main class="bg-zinc-800 w-full min-h-screen flex flex-col items-center justify-center" >
+            <span class="text-zinc-200">"Counter = " {count}</span>
+            <button
+                class="border border-zinc-700 p-2 rounded text-zinc-200"
+                class=("bg-blue-600", handleBlueClass)
+                class=("bg-red-600", handleRedClass)
+                on:click=move |_: MouseEvent| {
+                    set_count.update(|v| *v += 1);
+                }
+            >
+                Increment
+            </button>
         </main>
     }
 }
