50fdfd26ac9ab3193c0bad6fc782e25eb41a38ed
ouwargui <guikemon2@gmail.com>
2023-08-14 17:28:40 -0300

    feat: setup tailwindcss

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use leptos::*;
+use leptos::{*, ev::MouseEvent};
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
@@ -15,9 +15,14 @@ struct GreetArgs<'a> {
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    let increment = move |_: MouseEvent| set_count.update(|v| *v += 1);
+
     view! { cx,
-        <main class="bg-red-200" >
-            teste
+        <main class="bg-red-200 w-full min-h-screen flex flex-col items-center justify-center" >
+            <span>Counter = {count}</span>
+            <button on:click=increment class="border border-zinc-700 p-2 rounded bg-blue-300">Increment</button>
         </main>
     }
 }
