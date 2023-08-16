660deeed42a1a90d23849fcb353697f5a1d82a73
ouwargui <guikemon2@gmail.com>
2023-08-16 01:48:38 -0300

    feat: [wip] fetch, create and delete todos

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::Mutex;
+
 use serde::{Deserialize, Serialize};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -17,31 +19,86 @@ async fn load_asynchronous() -> Result<String, String> {
     Ok(format!("{}", body))
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Todo {
-    user_id: i32,
     id: i32,
     title: String,
     completed: bool,
 }
 
+impl Todo {
+    fn new(id: i32, title: &str, completed: bool) -> Self {
+        Self {
+            id,
+            title: title.to_string(),
+            completed,
+        }
+    }
+}
+
+struct Todos {
+    todos: Vec<Todo>,
+}
+
+impl Todos {
+    fn new() -> Self {
+        Self {
+            todos: vec![Todo {
+                id: 1,
+                title: "Learn Rust".to_string(),
+                completed: false,
+            }],
+        }
+    }
+
+    fn add(&mut self, todo: Todo) {
+        self.todos.push(todo);
+    }
+
+    fn get(&self) -> &Vec<Todo> {
+        &self.todos
+    }
+
+    fn remove(&mut self, todo_id: i32) {
+        self.todos.retain(|todo| todo.id != todo_id);
+    }
+}
+
+struct State {
+    todos: Mutex<Todos>,
+}
+
 #[tauri::command]
-async fn fetch_todos() -> Result<Vec<Todo>, String> {
-    let res = reqwest::get("https://jsonplaceholder.typicode.com/todos").await;
-    let body = res.unwrap().text().await.unwrap();
+async fn fetch_todos(state: tauri::State<'_, State>) -> Result<Vec<Todo>, String> {
+    let todos_unwrapped = state.todos.lock().unwrap();
+    let todos = todos_unwrapped.get();
+    Ok(todos.to_vec())
+}
 
-    let todos: Vec<Todo> = serde_json::from_str(&body).unwrap();
+#[tauri::command]
+async fn add_todo(todo: Todo, state: tauri::State<'_, State>) -> Result<(), String> {
+    let new_todo = Todo::new(todo.id, &todo.title, todo.completed);
+    state.todos.lock().unwrap().add(new_todo);
+    Ok(())
+}
 
-    Ok(todos)
+#[tauri::command]
+async fn remove_todo(id: i32, state: tauri::State<'_, State>) -> Result<(), String> {
+    state.todos.lock().unwrap().remove(id);
+    Ok(())
 }
 
 fn main() {
     tauri::Builder::default()
+        .manage(State {
+            todos: Mutex::new(Todos::new()),
+        })
         .invoke_handler(tauri::generate_handler![
             greet,
             load_asynchronous,
-            fetch_todos
+            fetch_todos,
+            add_todo,
+            remove_todo
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/async_todos.rs
+++ src/async_todos.rs
@@ -1,31 +1,54 @@
-use leptos::*;
+use leptos::{ev::{SubmitEvent, MouseEvent}, html::Input, *};
 use serde::{Deserialize, Serialize};
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
-    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
-}
+use tauri_sys::tauri;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(rename_all = "camelCase")]
 struct Todo {
-    user_id: i32,
     id: i32,
     title: String,
     completed: bool,
 }
 
+impl Todo {
+    fn new(id: i32, title: String, completed: bool) -> Self {
+        Self {
+            id,
+            title,
+            completed,
+        }
+    }
+}
+
 async fn fetch_todos() -> Vec<Todo> {
-    let todos_from_js = invoke("fetch_todos", JsValue::UNDEFINED).await;
-    let todos: Vec<Todo> = serde_wasm_bindgen::from_value(todos_from_js).unwrap();
+    let todos = tauri::invoke::<_, Vec<Todo>>("fetch_todos", &()).await.unwrap();
     log!("todos: {:?}", todos);
     todos
 }
 
+#[derive(Serialize, Deserialize)]
+struct AddTodo {
+    todo: Todo
+}
+
+async fn add_todo(todo: Todo) -> Result<(), ()> {
+    let res = tauri::invoke::<AddTodo, ()>("add_todo", &AddTodo { todo }).await.unwrap();
+    Ok(res)
+}
+
+#[derive(Serialize, Deserialize)]
+struct RemoveTodo {
+    id: i32
+}
+
+async fn remove_todo(id: i32) -> Result<(), ()> {
+    let res = tauri::invoke::<RemoveTodo, ()>("remove_todo", &RemoveTodo { id }).await.unwrap();
+    Ok(res)
+}
+
 #[component]
 pub fn AsyncTodos(cx: Scope) -> impl IntoView {
+    let (_counter, _set_counter) = create_signal(cx, 0);
+    let todo_title_input_ref = create_node_ref::<Input>(cx);
     let async_todos = create_resource(cx, || (), |_| async move { fetch_todos().await });
 
     let todos_result = move || {
@@ -36,12 +59,38 @@ pub fn AsyncTodos(cx: Scope) -> impl IntoView {
             .unwrap_or_else(|| vec![].into())
     };
 
+    let add_todo_action = create_action(cx, |input: &String| {
+        let input = input.to_owned();
+        let todo = Todo::new(0, input, false);
+        async move { add_todo(todo).await }
+    });
+
+    let add_todo = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        let input = todo_title_input_ref.get().expect("input to exist");
+        add_todo_action.dispatch(input.value());
+    };
+
+    let remove_todo_action = create_action(cx, |todo_id: &i32| {
+        let todo_id = todo_id.to_owned();
+        async move { remove_todo(todo_id).await }
+    });
+
+    let remove_todo = move |ev: MouseEvent, todo_id: i32| {
+        ev.prevent_default();
+        remove_todo_action.dispatch(todo_id);
+    };
+
     view! { cx,
-        <form class="flex flex-col gap-4 min-h-screen py-4">
+        <form
+            class="flex flex-col gap-4 min-h-screen py-4"
+            on:submit=add_todo
+        >
             <input
                 class="border border-zinc-200 rounded-md p-2 bg-inherit outline-none text-zinc-200"
                 type="text"
                 placeholder="What needs to be done?"
+                node_ref=todo_title_input_ref
             />
             <div class="text-zinc-200 ">
                 <h1 class="text-lg">"Your tasks:"</h1>
@@ -53,7 +102,11 @@ pub fn AsyncTodos(cx: Scope) -> impl IntoView {
                                     <span>
                                         {todo.title}
                                     </span>
-                                    <button class="ml-2 text-zinc-200" type="button">
+                                    <button
+                                        class="ml-2 text-zinc-200"
+                                        type="button"
+                                        on:click=move |ev| remove_todo(ev, todo.id)
+                                    >
                                         "X"
                                     </button>
                                 </div>
